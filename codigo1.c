5373dc2ffd57acb36e1affa6e2672421659711dd
Marko-eng72 <quispeccentemarko@gmail.com>
2026-09-09 18:41:54 -0500

    Add files via upload

---
+++ codigo1.c
@@ -0,0 +1,17 @@
+/*
+    NOTA: Compilar con  la siguiente linea, a finde moestrar ñ y acentos
+*/
+
+#include <stdio.h>
+
+int main() {
+    int edad = 18;
+    float altura = 1.75;
+    char inicial = 'M';
+
+    printf("¡Hola mundo en C!\n");
+    printf("Edad: %d años\n", edad);
+    printf("altura: %.2f\n", altura);
+    printf("inicial: %c\n", inicial);
+    return 0;
+}
