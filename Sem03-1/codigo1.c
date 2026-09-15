bbc12a4bf84ff26cc8b141869ad3dd31f15a679a
Marko-eng72 <quispeccentemarko@gmail.com>
2026-09-15 17:55:12 -0500

    Add files via upload

---
+++ Sem03-1/codigo1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a=12, b=10, c=5;
+
+    printf("Operadores aritmeticos\n");
+    printf("La suma de %d + %d es %d\n", a, b, a+b);
+    printf("la resta de %d - %d es %d\n", a, b, a-b);
+    printf("La multiplicacion de %d * %d es %d\n", a, b, a*b);
+
+    printf("¿%d > %d? es %d\n", a, b, a>b);
+    
+    return 0;
+}
